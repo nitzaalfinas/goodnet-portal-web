5e5e38cf0cb6848cf5bb94e63987e735c7c0cbd3
Nitza Alfinas <nitzaalfinas@gmail.com>
2025-10-14 08:15:01 +0700

    fix allowedhosts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["testnet-bridge.dexgood.com"],
+    allowedHosts: ["testnet-portal.dexgood.com"],
     proxy: {
       // Proxy RPC calls to avoid CORS issues in development
       '/api/rpc': {
