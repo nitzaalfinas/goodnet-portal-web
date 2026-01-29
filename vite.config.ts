c1b79db3700ef19e8630a1dc66524bd108d061f9
Nitza Alfinas <3109858+nitzaalfinas@users.noreply.github.com>
2026-01-29 16:03:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["testnet-portal.dexgood.com"],
+    allowedHosts: ["testnet-portal.dexgood.com","dev-good-portal.bizcopilot.app"],
     proxy: {
       // Proxy RPC calls to avoid CORS issues in development
       '/api/rpc': {
